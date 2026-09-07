dfbd65acee094b3b3f94b0cdc7b31d1a226d9a52
online111111 <github1@daski.online>
2026-09-07 12:09:41 +0800

    test: reuse shared round trip fixture

--- internal/cli/http_client_hardening_test.go
+++ internal/cli/http_client_hardening_test.go
@@ -8,10 +8,6 @@ import (
 	"testing"
 )
 
-type roundTripFunc func(*http.Request) (*http.Response, error)
-
-func (f roundTripFunc) RoundTrip(r *http.Request) (*http.Response, error) { return f(r) }
-
 func TestBearerTransportRejectsRemotePlainHTTPBeforeSending(t *testing.T) {
 	called := false
 	tr := bearerTransport{
